6096e41346d12c67689e7669571344f16e8af67f
BotRamenRobotics <redstonegamer360@gmail.com>
2022-05-09 12:22:00 -0400

    Added scenario code

---
+++ scenarios/Scenario.java
@@ -0,0 +1,10 @@
+package scenarios;
+
+public class Scenario {
+
+    private String setting; //Name of the place this scenario takes place
+    
+    public Scenario(String setting) {
+        this.setting = setting;
+    }
+}
